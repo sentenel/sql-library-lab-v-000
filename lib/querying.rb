02ab0ea51c0fdecfed7461917a42e9e0ab66ad0a
Seth Wax <sethlwax@gmail.com>
2019-01-03 10:43:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title FROM series
+  LEFT JOIN characters ON series.id = characters.series_id
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species)
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
