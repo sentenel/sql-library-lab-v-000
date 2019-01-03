0f5167ff45588af15353374e27f014559f8c9107
Seth Wax <sethlwax@gmail.com>
2019-01-03 10:44:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,21 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year;"
+  "SELECT title, year FROM books
+  WHERE series_id = 1
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
+  "SELECT name, motto FROM characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species) FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
